1805fd4402fe0907fc11dc790a71269dfc392a50
Yanis Plevokas <58010275+YanisPlevokas@users.noreply.github.com>
2021-04-12 10:49:24 +0300

    Add files via upload

---
+++ SecondSemesterLab_UI/Second_converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FirstSemesterLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace SecondSemesterLab_UI 
+{
+    class Second_converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value as DataItem? == null)
+                return "object error";
+            DataItem item = (DataItem)value;
+            return item.net.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SecondSemesterLab_UI/Third_converter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FirstSemesterLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace SecondSemesterLab_UI
+{
+    class Third_converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value as V4DataOnGrid == null)
+                return value?.ToString();
+            if (parameter as int? == null)
+                return "parameter error : " + parameter?.ToString() + " (" + parameter?.GetType() + ")";
+            int param = (int)parameter;
+            V4DataOnGrid data = (V4DataOnGrid)value;
+            double returned;
+            if (param < 0)
+            // Output min
+            {
+                returned = data.Aggregate((max, v) => v.electromagnet_field.Magnitude > max.electromagnet_field.Magnitude ? v : max).electromagnet_field.Magnitude;
+                return returned.ToString() + " - max magn";
+            }
+            else
+            // Output max
+            {
+                returned = data.Aggregate((min, v) => v.electromagnet_field.Magnitude < min.electromagnet_field.Magnitude ? v : min).electromagnet_field.Magnitude;
+                return returned.ToString() + " - min magn";
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
